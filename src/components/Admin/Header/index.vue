7600cda94f50c453e7ca5e823a799603fb08f0a2
zhcxk1998 <464203147@qq.com>
2019-12-11 21:01:26 +0800

    feat: 新增后台管理侧边栏和头部导航栏

---
+++ src/components/Admin/Header/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="admin__header">
+    <div class="header__logo">师大研学后台管理系统</div>
+    <div class="header__navbar"></div>
+    <div class="header__link"></div>
+  </div>
+</template>
+
+<style scoped>
+.admin__header {
+  position: fixed;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  width: 100vw;
+  height: 60px;
+  border: #d7d7d7 1px solid;
+  background: #fff;
+  z-index: 2;
+}
+
+.header__logo {
+  width: 200px;
+  height: 60px;
+  color: #333;
+  font-weight: bold;
+}
+
+.header__link {
+  margin-left: auto;
+}
+</style>
+
+<script>
+export default {
+  name: "Header",
+  data() {
+    return {
+      activeIndex: "1"
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
+};
+</script>
