81d5e0367bcca9c3ff4cb394dda723d0c5de8dce
zhcxk1998 <464203147@qq.com>
2019-12-14 13:22:51 +0800

    feat: 新增后台管理待审核机构详情页面

--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,8 @@ import Login from "@/pages/Login";
 import Index from '@/pages/Admin/Index/index'
 import Home from '@/pages/Admin/Home/index'
 import TabsManager from '@/pages/Admin/TabsManager/index'
-import AuditPending from '@/pages/Admin/AuditPending/index'
+import AuditPendingList from '@/pages/Admin/AuditPendingList/index'
+import AuditPendingDetail from '@/pages/Admin/AuditPendingDetail/index'
 
 Vue.use(VueRouter)
 
@@ -16,13 +17,14 @@ const routes = [
             { path: '/', redirect: 'home' },
             { path: 'home', component: Home },
             { path: 'tabs', component: TabsManager },
-            { path: 'audit-pending', component: AuditPending }
+            { path: 'audit-pending', component: AuditPendingList },
+            { path: 'audit-pending/:id', component: AuditPendingDetail }
         ]
     }
 
 ]
 
 export default new VueRouter({
-    mode: 'history',
+    mode: 'hash',
     routes
 })
