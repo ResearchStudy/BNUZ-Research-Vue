b2b4256981ac26e32416252f2a1609ea677db665
kingswwong <467271883@qq.com>
2019-12-17 00:54:01 +0800

    [Dean]: Add rules for login and register

--- src/pages/LoginOrRegister.vue
+++ src/pages/LoginOrRegister.vue
@@ -1,61 +1,73 @@
 <template>
-    <div>
-        <el-row>
-            <el-col :span="14" :offset="1"><div class="banner ">
-            </div></el-col>
-            <el-col :span="7" :offset="1"><div class="banner ">
-                <div style="text-align: center;font-size: 37px;font-weight: bold;padding-top: 95px">欢迎{{modulesName}}</div>
-                <div>
-                    <el-form :model="form" :rules="rules" ref="form" label-width="100px" style="width: 80%;padding: 30px">
-                        <el-form-item label="用户名" prop="key">
-                            <el-input v-model="form.key"></el-input>
-                        </el-form-item><el-form-item label="密码" prop="password">
-                            <el-input v-model="form.password" type="password"></el-input>
-                        </el-form-item>
-                        <div style="display: flex;justify-content: center;padding-left: 100px;">
-                            <el-button type="primary" style="width: 80%" @click="dispatchSubmit()">{{modulesName}}</el-button>
-                        </div>
-                    </el-form>
-                </div>
-            </div></el-col>
-        </el-row>
-    </div>
+  <div>
+    <el-row>
+      <el-col :span="14" :offset="1">
+        <div class="banner ">
+        </div>
+      </el-col>
+      <el-col :span="7" :offset="1">
+        <div class="banner ">
+          <div style="text-align: center;font-size: 37px;font-weight: bold;padding-top: 95px">欢迎{{modulesName}}</div>
+          <div>
+            <el-form :model="form" :rules="isLogin ? loginRules : registerRules" ref="form" label-width="100px" style="width: 80%;padding: 30px">
+              <el-form-item label="用户名" prop="key">
+                <el-input v-model="form.key"></el-input>
+              </el-form-item>
+              <el-form-item label="密码" prop="password">
+                <el-input v-model="form.password" :type="isLogin ? 'password' : 'text'"></el-input>
+              </el-form-item>
+              <div style="display: flex;justify-content: center;padding-left: 100px;">
+                <el-button type="primary" style="width: 80%" @click="dispatchSubmit()">{{modulesName}}</el-button>
+              </div>
+            </el-form>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
 </template>
 
 <script>
     import {login, register} from '@/api/user'
+
     export default {
         name: "LoginOrRegister",
-        data(){
+        data() {
             return {
                 form: {
                     key: '',
                     password: ''
                 },
-                rules: {
-                    key: [{ required: true, message: '请输入用户名', trigger: 'blur' }],
-                    password: [{ required: true, message: '请输入密码', trigger: 'blur' }],
+                loginRules: {
+                    key: [{required: true, message: '请输入用户名', trigger: 'blur'}],
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'}],
                 },
-                modulesName: this.$route.fullPath.replace("/", "") === 'login' ? '登录' : '注册'
+                registerRules: {
+                    key: [
+                        {required: true, message: '请输入用户名', trigger: 'blur'},
+                        {min: 6, max: 50, message: '长度在6到50之间', trigger: 'blur'}
+                    ],
+                    password: [{required: true, message: '请输入密码', trigger: 'blur'},
+                        {min: 8, max: 32, message: '长度在8到32之间', trigger: 'blur'}
+                    ],
+                },
+                modulesName: this.$route.fullPath.replace("/", "") === 'login' ? '登录' : '注册',
+                isLogin: this.$route.fullPath.replace("/", "") === 'login'
             }
         },
-        methods:{
-            login(){
+        methods: {
+            login() {
                 login({...this.form, remember: false}).then((res) => {
                     console.log(res)
                 })
             },
-            register(){
+            register() {
                 register({...this.form, username: this.form.key}).then((res) => {
                     console.log(res)
                 })
             },
-            dispatchSubmit(){
-                if(this.$route.fullPath.replace("/", "") === 'login'){
-                    this.login();
-                }else{
-                    this.register();
-                }
+            dispatchSubmit() {
+                this.isLogin ? this.login() : this.register()
             }
         }
     }
@@ -63,8 +75,8 @@
 
 <style scoped>
 
-.banner {
+  .banner {
     height: 500px;
     border: 1px solid black;
-}
+  }
 </style>
