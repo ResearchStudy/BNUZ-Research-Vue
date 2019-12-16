ffd64acb0a695e9a3bd26221c4b80412d05114ac
zhcxk1998 <464203147@qq.com>
2019-12-16 23:44:30 +0800

    feat: 新增后台管理用户管理列表模块

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import Home from '@/pages/Admin/Home/index'
 import TabsManager from '@/pages/Admin/TabsManager/index'
 import AuditPendingList from '@/pages/Admin/AuditPendingList/index'
 import AuditPendingDetail from '@/pages/Admin/AuditPendingDetail/index'
+import AccountsManager from '@/pages/Admin/AccountsManager/index'
 
 Vue.use(VueRouter);
 
@@ -15,15 +16,16 @@ const routes = [
         path: '/',
         component: index,
         children: [
-            {path: 'login', component: Login}
+            { path: 'login', component: Login }
         ]
     }, {
         path: '/admin', component: Index, children: [
-            {path: '/', redirect: 'home'},
-            {path: 'home', component: Home},
-            {path: 'tabs', component: TabsManager},
-            {path: 'audit-pending', component: AuditPendingList},
-            {path: 'audit-pending/:id', component: AuditPendingDetail}
+            { path: '/', redirect: 'home' },
+            { path: 'home', component: Home },
+            { path: 'tabs', component: TabsManager },
+            { path: 'audit-pending', component: AuditPendingList },
+            { path: 'audit-pending/:id', component: AuditPendingDetail },
+            { path: 'accounts', component: AccountsManager }
         ]
     }
 
