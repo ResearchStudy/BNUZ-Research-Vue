66d9a87580cfe67ae2900d5e277b7e9af8117202
zhcxk1998 <464203147@qq.com>
2019-12-21 23:16:36 +0800

    feat: 新增后台管理机构个人资料修改功能

--- src/pages/InstitutionAdmin/DashBoard/index.vue
+++ src/pages/InstitutionAdmin/DashBoard/index.vue
@@ -8,18 +8,24 @@
     <div class="institution-admin-dashboard__main">
       <div class="institution-admin-dashboard__wrap">
         <div class="institution-admin-dashboard__title">基本资料</div>
-        <el-form ref="form" label-width="120px" label-suffix=":">
-          <el-form-item label="企业名称">
+        <el-form
+          ref="institutionDetails"
+          :model="institutionDetails"
+          :rules="rules"
+          label-width="120px"
+          label-suffix=":"
+        >
+          <el-form-item label="企业名称" prop="name">
             <div class="content">
               <el-input v-model="institutionDetails.name"></el-input>
             </div>
           </el-form-item>
-          <el-form-item label="企业注册号">
+          <el-form-item label="企业注册号" prop="tax_id">
             <div class="content">
               <el-input v-model="institutionDetails.tax_id"></el-input>
             </div>
           </el-form-item>
-          <el-form-item label="企业类型">
+          <el-form-item label="企业类型" prop="institution_type">
             <div class="content">
               <el-input v-model="institutionDetails.institution_type"></el-input>
             </div>
@@ -27,21 +33,46 @@
           <div style="display:flex">
             <el-form-item label="省份" style="width:50%">
               <div class="content">
-                <el-input v-model="institutionDetails.province_name"></el-input>
+                <el-select
+                  v-model="institutionDetails.province_id"
+                  placeholder="请选择省份"
+                  filterable
+                  @change="handleProvinceChange"
+                  value-key="id"
+                >
+                  <el-option
+                    v-for="item in provinceList"
+                    :key="item.id"
+                    :label="item.name"
+                    :value="item.id"
+                  ></el-option>
+                </el-select>
               </div>
             </el-form-item>
             <el-form-item label="城市" style="width:50%">
               <div class="content">
-                <el-input v-model="institutionDetails.city_name"></el-input>
+                <el-select
+                  v-model="institutionDetails.city_id"
+                  filterable
+                  value-key="id"
+                  placeholder="请选择城市"
+                >
+                  <el-option
+                    v-for="item in cityList"
+                    :key="item.id"
+                    :label="item.name"
+                    :value="item.id"
+                  ></el-option>
+                </el-select>
               </div>
             </el-form-item>
           </div>
-          <el-form-item label="详细地址">
+          <el-form-item label="详细地址" prop="details">
             <div class="content">
               <el-input v-model="institutionDetails.details"></el-input>
             </div>
           </el-form-item>
-          <el-form-item label="法定代表人">
+          <el-form-item label="法定代表人" prop="legal_person">
             <div class="content">
               <el-input v-model="institutionDetails.legal_person"></el-input>
             </div>
@@ -54,48 +85,33 @@
                 placeholder="成立时间"
                 value-format="timestamp"
                 style="width: 100%;"
+                :clearable="false"
+                :editable="false"
               ></el-date-picker>
             </div>
           </el-form-item>
-          <el-form-item label="注册资金">
+          <el-form-item label="注册资金" prop="registered_money">
             <div class="content">
               <el-input v-model="institutionDetails.registered_money"></el-input>
             </div>
           </el-form-item>
           <el-form-item label="营业期限">
             <div class="content">
-              <!-- <el-col :span="11">
-                <el-date-picker
-                  type="date"
-                  v-model="institutionDetails.business_license_start_time"
-                  value-format="timestamp"
-                  placeholder="开始时间"
-                  style="width: 100%;"
-                ></el-date-picker>
-              </el-col>
-              <el-col class="line" :span="2" style="text-align:center">-</el-col>
-              <el-col :span="11">
-                <el-date-picker
-                  type="date"
-                  v-model="institutionDetails.business_license_ent_time"
-                  value-format="timestamp"
-                  placeholder="结束时间"
-                  style="width: 100%;"
-                ></el-date-picker>
-              </el-col>-->
               <el-date-picker
                 style="width:100%"
                 unlink-panels
-                v-model="timeRange"
+                v-model="institutionDetails.timeRange"
                 value-format="timestamp"
                 type="daterange"
                 range-separator="至"
                 start-placeholder="开始日期"
                 end-placeholder="结束日期"
+                :clearable="false"
+                :editable="false"
               ></el-date-picker>
             </div>
           </el-form-item>
-          <el-form-item label="登记机关">
+          <el-form-item label="登记机关" prop="registration_authority">
             <div class="content">
               <el-input v-model="institutionDetails.registration_authority"></el-input>
             </div>
@@ -108,15 +124,17 @@
                 value-format="timestamp"
                 placeholder="核准时间"
                 style="width: 100%;"
+                :clearable="false"
+                :editable="false"
               ></el-date-picker>
             </div>
           </el-form-item>
-          <el-form-item label="联系电话">
+          <el-form-item label="联系电话" prop="phone">
             <div class="content">
               <el-input v-model="institutionDetails.phone"></el-input>
             </div>
           </el-form-item>
-          <el-form-item label="经营范围">
+          <el-form-item label="经营范围" prop="business_scope">
             <div class="content">
               <el-input type="textarea" v-model="institutionDetails.business_scope"></el-input>
             </div>
@@ -127,18 +145,15 @@
                 class="avatar-uploader"
                 action="https://jsonplaceholder.typicode.com/posts/"
                 :show-file-list="false"
-                :on-success="handleAvatarSuccess"
                 :before-upload="beforeAvatarUpload"
-                :on-progress="handleAvatarProgress"
+                :http-request="handleAvatarUpload"
               >
-                <el-progress
-                  class="upload-progress"
-                  v-if="isUploading"
-                  type="circle"
-                  :percentage="uploadPercent"
-                ></el-progress>
-                <img v-if="imageUrl && !isUploading" :src="imageUrl" class="avatar" />
-                <i v-if="!imageUrl && !isUploading" class="el-icon-plus avatar-uploader-icon"></i>
+                <img
+                  v-if="institutionDetails.imageUrl"
+                  :src="institutionDetails.imageUrl"
+                  class="avatar"
+                />
+                <i v-if="!institutionDetails.imageUrl" class="el-icon-plus avatar-uploader-icon"></i>
               </el-upload>
             </div>
           </el-form-item>
@@ -148,25 +163,20 @@
             </div>
           </el-form-item>
           <div v-if="isMore">
-            <el-form-item label="发票抬头">
+            <el-form-item label="发票抬头" prop="invoice_rise">
               <div class="content">
                 <el-input v-model="institutionDetails.invoice_rise"></el-input>
               </div>
             </el-form-item>
-            <el-form-item label="纳税人识别号">
+            <el-form-item label="纳税人识别号" prop="taxpayer_distinguish">
               <div class="content">
                 <el-input v-model="institutionDetails.taxpayer_distinguish"></el-input>
-              </div>
-            </el-form-item>
-            <el-form-item label="电话号码">
-              <div class="content">
-                <el-input v-model="institutionDetails.phone"></el-input>
               </div>
             </el-form-item>
           </div>
           <el-form-item>
             <div class="content">
-              <el-button type="primary" @click="handleSaveClick">保存</el-button>
+              <el-button type="primary" @click="handleSaveClick('institutionDetails')">保存</el-button>
             </div>
           </el-form-item>
         </el-form>
@@ -176,31 +186,30 @@
 </template>
 
 <script>
+import ruleList from "./validate";
 export default {
   name: "InsitutionAdminDashBoard",
   data() {
     return {
-      id: -1,
+      institutionId: -1,
       isMore: false,
-      isUploading: false,
+      isUploaded: false,
       institutionDetails: {},
-      timeRange: [],
-      imageUrl: "",
-      uploadPercent: 0
+      provinceList: [],
+      cityList: [],
+      addressList: [],
+      preProvinceId: -1,
+      preCityId: -1,
+      rules: ruleList
     };
   },
   async mounted() {
-    const {
-      getters: { userInfo }
-    } = this.$store;
-    const { id } = userInfo;
-    this.id = id;
-    await this.getUserInfo();
+    await this.getInstitutionInfo();
   },
   methods: {
-    async getUserInfo() {
+    async getInstitutionInfo() {
       const {
-        data: { institution_details }
+        data: { institution_details, id }
       } = await this.$http.get(`/api/institutions/me`);
       const {
         establish_time,
@@ -208,59 +217,146 @@ export default {
         business_license_start_time,
         business_license_ent_time,
         logo,
-        address: { province_name, city_name, details }
+        address: { details, province_name, city_name }
       } = institution_details;
+      this.institutionId = id;
+
+      await this.getProvinceList();
+      const provinceId = this.provinceList.filter(
+        province => province.name === province_name
+      )[0].id;
+      await this.getCityList(provinceId);
+      const cityId = this.cityList.filter(city => city.name === city_name)[0]
+        .id;
+
+      this.preProvinceId = provinceId;
+      this.preCityId = cityId;
       this.institutionDetails = {
         ...institution_details,
         establish_time: establish_time * 1000,
         approval_time: approval_time * 1000,
-        province_name,
-        city_name,
-        details
+        details,
+        province_id: provinceId,
+        city_id: cityId,
+        timeRange: [
+          business_license_start_time * 1000,
+          business_license_ent_time * 1000
+        ],
+        imageUrl: "/api/resources/" + logo
       };
-      this.timeRange = [
-        business_license_start_time * 1000,
-        business_license_ent_time * 1000
-      ];
-      this.imageUrl = "/api/resources/" + logo;
     },
-    async handleSaveClick() {
-      const [
-        business_license_start_time,
-        business_license_ent_time
-      ] = this.timeRange;
+    handleSaveClick(formName) {
+      this.$refs[formName].validate(async valid => {
+        if (valid) {
+          const [
+            business_license_start_time,
+            business_license_ent_time
+          ] = this.institutionDetails.timeRange;
+          const {
+            name,
+            invoice_rise,
+            approval_time,
+            taxpayer_distinguish,
+            phone,
+            address_id,
+            institution_type,
+            establish_time,
+            legal_person,
+            registered_money,
+            registration_authority,
+            business_scope,
+            province_id,
+            city_id,
+            details,
+            imageUrl
+          } = this.institutionDetails;
+
+          const isAddressChange =
+            this.preProvinceId !== province_id || this.preCityId !== city_id;
+
+          if (isAddressChange) {
+            const {
+              data: { id: newAddressId }
+            } = await this.$http.post("/api/address", {
+              country_id: 1,
+              province_id,
+              city_id,
+              details
+            });
+            this.institutionDetails.address_id = newAddressId;
+          }
+
+          const logoInfo = this.isUploaded
+            ? {
+                logo: imageUrl
+              }
+            : {};
+          await this.$http.put(`/api/institutions/${this.institutionId}`, {
+            name,
+            invoice_rise,
+            taxpayer_distinguish,
+            phone,
+            address_id,
+            approval_time: approval_time / 1000,
+            business_license_start_time: business_license_start_time / 1000,
+            business_license_ent_time: business_license_ent_time / 1000,
+            institution_type,
+            establish_time: establish_time / 1000,
+            legal_person,
+            registered_money: parseFloat(registered_money),
+            registration_authority,
+            business_scope,
+            province_id,
+            city_id,
+            ...logoInfo
+          });
+          this.$message({
+            type: "success",
+            message: "修改成功"
+          });
+        } else {
+          this.$message({
+            type: "error",
+            message: "请按要求完成表格"
+          });
+          return false;
+        }
+      });
+    },
+    async getProvinceList() {
       const {
-        name,
-        invoice_rise,
-        taxpayer_distinguish,
-        phone,
-        address_id,
-        logo
-      } = this.institutionDetails;
-      console.log(
-        name,
-        invoice_rise,
-        taxpayer_distinguish,
-        phone,
-        address_id,
-        logo,
-        business_license_start_time,
-        business_license_ent_time,
-      );
+        data: { address: provinceList }
+      } = await this.$http.get("/api/address/list", {
+        target: 2
+      });
+      this.provinceList = provinceList;
     },
-    handleAvatarProgress(event) {
-      this.isUploading = true;
-      this.uploadPercent = parseInt(event.percent.toFixed(0));
+    async getCityList(provinceId) {
+      const {
+        data: { address: cityList }
+      } = await this.$http.get("/api/address/list", {
+        target: 3,
+        parent: provinceId
+      });
+      this.cityList = cityList;
     },
-    handleAvatarSuccess(res, file) {
-      this.isUploading = false;
-      this.uploadPercent = 0;
-      // this.imageUrl = URL.createObjectURL(file.raw);
-      const reader = new FileReader();
-      reader.readAsDataURL(file.raw);
-      reader.onload = e => {
-        this.imageUrl = e.target.result;
-      };
+    async handleProvinceChange(provinceId) {
+      this.institutionDetails.province_id = provinceId;
+      await this.getCityList(provinceId);
+      this.institutionDetails.city_id = this.cityList[0].id;
+    },
+    async handleAvatarUpload({ file }) {
+      this.isUploaded = true;
+      this.institutionDetails.imageUrl = await this.getImageInfo(file);
+    },
+    getImageInfo(file) {
+      return new Promise(async resolve => {
+        const reader = new FileReader();
+        reader.readAsDataURL(file);
+        reader.onload = e => {
+          resolve(e.target.result);
+        };
+      });
     },
     beforeAvatarUpload(file) {
       const isJPG = file.type === "image/jpeg";
@@ -286,10 +382,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.institution-admin-dashboard {
-  &__container {
-  }
+@import "./recover.scss";
 
+.institution-admin-dashboard {
   &__title {
     margin-bottom: 30px;
     color: #333;
@@ -309,7 +404,6 @@ export default {
     padding: 10px;
 
     .content {
-      // max-width: 50%;
       margin-left: 30px;
       color: #666;
 
@@ -319,30 +413,6 @@ export default {
         align-items: center;
         width: 178px;
         height: 178px;
-      }
-
-      /deep/ .avatar-uploader .el-upload {
-        border: 1px dashed #d9d9d9;
-        border-radius: 6px;
-        cursor: pointer;
-        position: relative;
-        overflow: hidden;
-      }
-      /deep/ .avatar-uploader .el-upload:hover {
-        border-color: #409eff;
-      }
-      /deep/ .avatar-uploader-icon {
-        font-size: 28px;
-        color: #8c939d;
-        width: 178px;
-        height: 178px;
-        line-height: 178px;
-        text-align: center;
-      }
-      /deep/ .avatar {
-        width: 178px;
-        height: 178px;
-        display: block;
       }
     }
 
@@ -354,11 +424,6 @@ export default {
       background-size: cover;
       background-repeat: no-repeat;
       background-position: center;
-      // background-image: linear-gradient(120deg, #a1c4fd 0%, #c2e9fb 100%);
-    }
-
-    /deep/ .el-form-item {
-      margin-bottom: 16px;
     }
   }
 }
