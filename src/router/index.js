bebd4c02f9b4c3e1689f708508b3ee869b6294b5
HUANGDE4 <dean.huang@cargosmart.com>
2020-01-02 14:59:00 +0800

    [Dean]: Fix route auth issues

--- src/router/index.js
+++ src/router/index.js
@@ -62,7 +62,7 @@ const router = new VueRouter({
     routes
 });
 
-const permitAllRoutes = ['/login', '/register', '/home', '/organization','/courses','/informations'];
+const permitAllRoutes = ['/login', '/register', '/', '/organization','/courses','/informations'];
 
 
 router.beforeEach((to, from, next) => {
@@ -72,6 +72,20 @@ router.beforeEach((to, from, next) => {
         store.dispatch('setRole', "")
     }
     if (permitAllRoutes.includes(to.path) || store.getters.role.length !== 0) {
+        if(from.fullPath === '/login' && localStorage.getItem("id")){
+            getUserInfo(localStorage.getItem("id")).then((res) => {
+                store.dispatch('setUserInfoAndRole', res);
+                if (res.role === 99) {
+                    router.addRoutes(rootAdminRoutes);
+                } else if (res.role === 8) {
+                    router.addRoutes(insitutionAdminRoutes)
+                }
+                else if (res.role === 0 || res.role === 1 || res.role === 2) {
+                    router.addRoutes(normalRoutes);
+                }
+                next({ path: to.path })
+            })
+        }
         next()
     }
     else {
