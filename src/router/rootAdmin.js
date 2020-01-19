dd42cdc37f39f40876e8245a92a8c0a527ebd177
zhcxk1998 <464203147@qq.com>
2020-01-19 22:47:40 +0800

    refactor: 修改标签管理组件名及路由名

--- src/router/rootAdmin.js
+++ src/router/rootAdmin.js
@@ -1,6 +1,6 @@
 import Index from '@/pages/RootAdmin/Index/index'
 import Home from '@/pages/RootAdmin/Home/index'
-import TabsManager from '@/pages/RootAdmin/TabsManager/index'
+import TagsManager from '@/pages/RootAdmin/TagsManager/index'
 import AuditPendingList from '@/pages/RootAdmin/AuditPendingList/index'
 import AuditPendingDetail from '@/pages/RootAdmin/AuditPendingDetail/index'
 import AuditPendingModify from '@/pages/RootAdmin/AuditPendingModify/index'
@@ -15,7 +15,7 @@ const adminRoutes = [
     path: '/root-admin', component: Index, children: [
       { path: '/', redirect: 'dashboard' },
       { path: 'dashboard', component: Home },
-      { path: 'tabs', component: TabsManager },
+      { path: 'tags', component: TagsManager },
       { path: 'audit-pending-list', component: AuditPendingList },
       { path: 'audit-pending-list/:id', component: AuditPendingDetail },
       { path: 'audit-pending-modify/', component: AuditPendingModify },
