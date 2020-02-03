44526a6774b1025dbed981550636cf941b463ede
zhcxk1998 <464203147@qq.com>
2020-02-03 15:31:25 +0800

    style: 优化课程列表卡片的样式

--- src/components/courses/card.vue
+++ src/components/courses/card.vue
@@ -1,41 +1,87 @@
 <template>
-<div style="border: 1px solid #ffffff;padding: 20px;background-color: white;overflow: hidden;display: inline-block">
-<div style="cursor: pointer;border: 1px solid #f2f2f2; box-shadow: 0 0 10px 0 rgba(0,0,0,0.1)">
-  <img :src="imgSrc" alt="courseIcon" style="width: 100%;height: 150px" v-if="imgSrc !== '/api/resources/'">
-  <img src="../../assets/img/default-news.jpg" alt="courseIcon" style="width: 100%;height: 150px" v-else>
-  <div style="padding-left: 5%;width: 100%;height: 80px;margin-top: 10px" >
-    <el-link :underline="false" style="font-size: 16px;font-weight: bold">{{title.length > 10 ? title.substring(0, 10) + '...' :title}}</el-link>
-    <div style="font-size: 14px;margin-top: 10px;width: 90%">  {{description.length > 28 ? description.substring(0, 28) + '...' : description}}  </div>
+  <div>
+    <div
+      class="card__img"
+      v-if="imgSrc !== '/api/resources/'"
+      :style="`background-image: url(${imgSrc})`"
+      alt="courseIcon"
+    />
+    <div class="card__img card__img--default" v-else alt="courseIcon" />
+    <div class="card__info">
+      <div class="title">{{title}}</div>
+      <div class="description">{{description}}</div>
+    </div>
   </div>
-</div>
-
-</div>
 </template>
 
 <script>
-    export default {
-        name: "card",
-        props:{
-            title: {
-                type: String,
-                default: ''
-            },
-          description: {
-                type: String,
-                default: ''
-            },
-            tagName:{
-                type: String,
-                default: ''
-            },
-            imgSrc: {
-                type: String,
-                default: ''
-            }
-        }
+export default {
+  name: "card",
+  props: {
+    title: {
+      type: String,
+      default: ""
+    },
+    description: {
+      type: String,
+      default: ""
+    },
+    tagName: {
+      type: String,
+      default: ""
+    },
+    imgSrc: {
+      type: String,
+      default: ""
     }
+  }
+};
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+.card {
+  &__img {
+    width: 100%;
+    height: 150px;
+    background-size: cover;
+    background-repeat: no-repeat;
 
+    &--default {
+      background-image: url(../../assets/img/default-news.jpg);
+    }
+  }
+
+  &__info {
+    height: 80px;
+    padding: 5px;
+
+    .title {
+      overflow: hidden;
+      width: 240px;
+      height: 30px;
+      padding: 0 5px;
+      font-size: 16px;
+      font-weight: bold;
+      text-decoration: none;
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      transition: color 0.3s ease-in-out;
+
+      &:hover {
+        color: #1890ff;
+      }
+    }
+
+    .description {
+      overflow: hidden;
+      display: -webkit-box;
+      height: 40px;
+      padding: 0 5px;
+      font-size: 14px;
+      text-overflow: ellipsis;
+      -webkit-box-orient: vertical;
+      -webkit-line-clamp: 2;
+    }
+  }
+}
 </style>
